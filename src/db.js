27a5ad334eca1314a691b46d28f4069b6e99a66f
Fernando0Murillo <86390468+Fernando0Murillo@users.noreply.github.com>
2023-10-16 12:55:46 -0600

    .

--- src/db.js
+++ src/db.js
@@ -1,9 +1,20 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPort = process.env.DB_PORT;
+const dbPass = process.env.DB_PASSWORD;
+const dbData = process.env.DB_DATABASE;
+
 
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: 'wfECVj0X0aJQ8ams',
-    database: 'db_industria'
+    
+    host: dbHost,
+    user: dbUser,
+    port: dbPort,
+    password: dbPass,
+    database: dbData
+    
 })
