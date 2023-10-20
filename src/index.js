0f880c05afa4fd96ecad1f4671e99241d6d81c05
Jorge Orellana <1998jorgeorellana@gmail.com>
2023-10-20 17:08:41 -0600

    ...

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ app.listen(3000);
 //Cofiguración de cors para que establesca la conexión solo en este dominio
 app.use(cors(
     {
-        origin: 'http://localhost:5174',
+        origin: 'http://localhost:5173',
         credentials: true
     }
 ));
 app.use(express.json())
 app.use(cookieParser());
-console.log('Server en el puerto ',3000)
+console.log('Server en el puerto ', 3000)
 
 app.use('/', routes);
